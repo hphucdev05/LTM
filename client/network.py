e8a85ca9abcb016436e66f05fd946006716519b4
Huỳnh Minh Quân <166901632+quanhocdev@users.noreply.github.com>
2025-12-24 23:24:16 +0700

    Bổ sung thêm hàm kết nối đến server
    Added connect method to establish server connection.

--- client/network.py
+++ client/network.py
@@ -5,10 +5,21 @@ import json
 from .constants import SERVER_IP, PORT
 class NetworkClient:
     def __init__(self):
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)     // Tạo socket IPv4, TCP
-        self.client.settimeout(3)     // Timeout 3 giây khi connect để tránh bị treo
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)     # Tạo socket IPv4, TCP
+        self.client.settimeout(3)     # Timeout 3 giây khi connect để tránh bị treo
         self.connected = False  
         self.my_id = None
         self.room_id = None
         self.turn = None
-        self.msg_queue = []    // hàng đợi mess nhận từ server
+        self.msg_queue = []    # hàng đợi mess nhận từ server
+
+    # Kết nối tới server
+    def connect(self):
+        try:
+            self.client.connect((SERVER_IP, PORT))
+            self.client.settimeout(None)  # Tắt timeout sau khi kết nối thành công
+            self.connected = True
+            threading.Thread(target=self.receive_loop, daemon=True).start()    #chạy luồng riêng nhận dữ liệu
+            return True
+        except: return False
+        #lỗi thì ném lỗi và trả về false
