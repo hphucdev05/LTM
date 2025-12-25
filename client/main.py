ba27c72000d152da1cf8ca5d647f891de22b0cd2
hphucdev05 <hphucdev05@gmail.com>
2025-12-25 11:15:14 +0700

    Tao main

---
+++ client/main.py
@@ -0,0 +1,12 @@
+# client/main.py
+import pygame
+import sys
+import os
+
+# Import modules
+from client.constants import *
+from client.network import NetworkClient
+from client.game_logic import ShipObj, check_valid_placement, reset_board
+import client.ui as ui
+if __name__ == "__main__":
+    pass()
